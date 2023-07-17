76779fde2814d8a387b9d607588416a22f71d2e0
somexotic <somexotic@yandex.ru>
2023-07-17 15:26:41 +0300

    Задача выполена

---
+++ Zadanie1/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Введите строку");
+string str = Console.ReadLine();
+
+if (str.Length % 2 == 0)
+{
+    string str1 = "", str2 = "";
+    for (int i = 0; i < str.Length; i++)
+    {
+        if (i < str.Length / 2)
+        {
+            str1 += str[i];
+        }
+        else
+        {
+            str2 += str[i];
+        }
+    }
+    string newStr = workWithStr.ReveseStr(str1) + workWithStr.ReveseStr(str2);
+    Console.WriteLine(newStr);
+}
+else
+{
+    string revStr = workWithStr.ReveseStr(str);
+    string newStr = revStr + str;
+    Console.WriteLine(newStr);
+}
+public static class workWithStr
+{
+    public static string ReveseStr(string qwe)
+    {
+        char[] arrStr = qwe.ToCharArray();
+        Array.Reverse(arrStr);
+        return new string(arrStr);
+    }
+}
